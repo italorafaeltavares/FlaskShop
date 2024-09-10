2c32bc4d46292a62e7692fee0f9b2e48716acafc
italorafaeltavares <italorafaeltavares@gmail.com>
2024-09-10 19:49:09 -0300

    feat: created connection with database

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecomerce.db'
+
+db = SQLAlchemy(app)
+
+# Modelagem 
+# Produto (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 # Defini a rota raiz e a função que será executada ao requisitar
 @app.route('/')
